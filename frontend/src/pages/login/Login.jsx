bd0f4ff9e095d0664eb696c010c5b7aff9d5798f
Povindu Samarasekara <povinduchanmith@gmail.com>
2024-09-10 17:54:32 +0530

    feat: socket feat

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -17,8 +17,9 @@ const Login = () => {
 		<div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
 			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
 				<h1 className='text-3xl font-semibold text-center text-gray-300'>
+				<span className='text-orange-500'> ChatSpark</span> <br/> <br/>
 					Login
-					<span className='text-blue-500'> ChatApp</span>
+					
 				</h1>
 
 				<form onSubmit={handleSubmit}>
